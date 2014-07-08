f100342d63b60a425ed35f31bc9faae708b4ab9c
Nagarjuna <ynagarjuna2012@gmail.com>
2014-07-09 04:52:36 +0530

    abouts and homepage

--- app/views/pages/aboutus.blade.php
+++ app/views/pages/aboutus.blade.php
@@ -5,5 +5,29 @@
 	<div class="cover">
 		<div id="aboutuscover"></div>
 	</div>
+	
+
+<p id="title1">Clickntoor was created to make students life Easy,Fun and Enjoyable</p>
+
+		<div id="aboutus-aboutus">
+<div class="aboutus">{{HTML::image('assets/aboutus/aboutuspicsall-01.png','alt_eqvialent_in_html',array('class'=>'about-img'))}}<span class="about-img-caption">Follow friends for
+trusted reviews</span></div>
+<div class="aboutus" id="page1-about2">{{HTML::image('assets/aboutus/aboutuspicsall-02.png','alt_eqvialent_in_html',array('class'=>'about-img'))}}<span class="about-img-caption">See comments,Reviews
+and share your travel experiences
+with your friends</span></div>
+<div class="aboutus">{{HTML::image('assets/aboutus/aboutuspicsall-06.png','alt_eqvialent_in_html',array('class'=>'about-img'))}}<span class="about-img-caption">Find Exiciting places
+around you with a click</span></div>
+<div class="aboutus">{{HTML::image('assets/aboutus/aboutuspicsall-03.png','alt_eqvialent_in_html',array('class'=>'about-img'))}}<span class="about-img-caption">Invite your friends
+of other colleges for trips</span></div>
+<div class="aboutus">{{HTML::image('assets/aboutus/aboutuspicsall-04.png','alt_eqvialent_in_html',array('class'=>'about-img'))}}<span class="about-img-caption">We will take care of your
+travel and stay</span></div>
+<div class="aboutus">{{HTML::image('assets/aboutus/aboutuspicsall-05.png','alt_eqvialent_in_html',array('class'=>'about-img'))}}<span class="about-img-caption">Use our responsive
+website with ease</span></div>
+</div>
+
+<div id="page7">
+	<a href="#">{{HTML::image('assets/twitter.jpg','twitter',array('id'=>'twitter'))}}</a><span id="tweet">follow us on twitter</span>
+</div>	
+	</div>
 
 @stop
--- app/views/pages/home.blade.php
+++ app/views/pages/home.blade.php
@@ -10,14 +10,65 @@
 	<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
 	{{ stylesheet_link_tag() }}
     {{ javascript_include_tag() }}
+		<script>
+    $(function() {
+	    $("#home-cover").vegas({
+             src:'aboutus.jpg'
+        });
+         $("home-cover").vegas('overlay', {
+             src:'overlays/13.png'
+       });
+    });
 
+    </script>
+
+    <style>
+    #home-cover{width: 100%;height: 640px;}
+    </style>
     </head>
 
   <body>
+	<hr id="top">
 
-  Write the html content here and create corresponding css files in assets
-  note: use different clasees or id's in every page
+<div id="home-cover">
 
+	<div class="col-xs-1"></div>
+	<a href="#"  id="logo-anchor" ">{{HTML::image('assets/logobot.png','logo',array('id'=>'logo'))}}</a>
+  <ul class="list-inline" id="nav-top">
+    <li><a href="#">LOGIN</a></li>
+    <li><a href="#">SIGN UP</a></li>
+    <li><a href="#">JOIN OUR TEAM</a></li>
+  </ul>  
+
+<div class="col-xs-12"><h2>Discover great places to hangout with your friends around your college </h2></div>
+<!--
+<form class="form-inline">
+       
+        <a href="#"><div class="input-group">
+          <input type="text" class="form-control input-lg" placeholder="Search...">
+          <div class="input-group-addon">
+            <span class="glyphicon glyphicon-search"></span>
+          </div>
+        </div></a>
+       
+</form>
+-->
+<div id="launch">We are launching on July 26</div>
+</div>
+	
+  <div id="home-about-container">
+  	<div id="home-about">
+  		<div class="home-about-box" id="home-about-box1">{{HTML::image('assets/aboutus/aboutuspicsall-01.png','Follow Freinds fortrusted reviews',array('class'=>'home-about-img','id'=>'home-about-img1'))}}<span class="home-about-img-caption">Follow friends for
+             trusted reviews</span></div>
+        <div class="home-about-box" id="home-about-box2">{{HTML::image('assets/aboutus/aboutuspicsall-02.png','share ,comment',array('class'=>'home-about-img','id'=>'home-about-img2'))}}<span class="home-about-img-caption">See comments, Reviews
+             and share your travel experiences with your friends</span></div>
+        <div class="home-about-box" id="home-about-box3">{{HTML::image('assets/aboutus/aboutuspicsall-03.png','Invite your friends',array('class'=>'home-about-img','id'=>'home-about-img3'))}}<span class="home-about-img-caption">Invite your friends
+             of other colleges for trips</span></div>
+
+        <a href="#"><div id="fb-login">{{HTML::image('assets/fb.png','fb',array('id'=>'fb'))}}<span id="fb-text">LOGIN WITH FACEBOOK</span></div></a>
+    </div>
+  </div>
+ 
 
 {{-- This is a multi-line comment in blade template 
 
