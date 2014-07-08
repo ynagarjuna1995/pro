e7e564a7794c744c2dea53dcdf64e93916d9bd31
Nagarjuna <ynagarjuna2012@gmail.com>
2014-07-08 11:37:50 +0530

    removed way in app/config

--- app/config/app.php
+++ app/config/app.php
@@ -122,7 +122,7 @@ return array(
 		'Illuminate\View\ViewServiceProvider',
 		'Illuminate\Workbench\WorkbenchServiceProvider',
 		'Codesleeve\AssetPipeline\AssetPipelineServiceProvider',
-		'Way\Generators\GeneratorsServiceProvider',
+		
 
 	),
 
