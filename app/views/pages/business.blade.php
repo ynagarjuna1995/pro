de4870dc4156f6eb54bceb6b223b2e88ac881dad
Nagarjuna <ynagarjuna2012@gmail.com>
2014-07-10 02:15:39 +0530

    Business and Careers

--- app/views/pages/business.blade.php
+++ app/views/pages/business.blade.php
@@ -2,8 +2,40 @@
 
 @section('title','Business Listing')
 @section('content')
-	<div class="cover">
+	<div id="business-cover">
 		<div id="coverbusiness"></div>
-	</div>
+	
+		 <p id="business-title1">Clickntoor is an initiative to organize hastle free trips to places near you</p>
 
+<div id="business-about-container">
+<div class="business-about">{{HTML::image('assets/business/businesspage_for_wesbite-08.png','alt_eqvialent_in_html',array('class'=>'business-about-img'))}}<span class="business-about-img-caption">List your travel business on clickntoor</span></div>
+<div class="business-about" id="business-about2">{{HTML::image('assets/business/businesspage_for_wesbite-06.png','alt_eqvialent_in_html',array('class'=>'business-about-img'))}}<span class="business-about-img-caption">Upload pictures,Information about tour packages and attract customers with special offers</span></div>
+<div class="business-about">{{HTML::image('assets/business/businesspage_for_wesbite-05.png','alt_eqvialent_in_html',array('class'=>'business-about-img'))}}<span class="business-about-img-caption">Target varied audience Get noticed by vast student community on clickntoor</span></div>
+<div class="business-about">{{HTML::image('assets/business/businesspage_for_wesbite-03.png','alt_eqvialent_in_html',array('class'=>'business-about-img'))}}<span class="business-about-img-caption">Reply to customer reviews and feedback. Interact with existing potential customers</span></div>
+<div class="business-about">{{HTML::image('assets/business/businesspage_for_wesbite-07.png','alt_eqvialent_in_html',array('class'=>'business-about-img'))}}<span class="business-about-img-caption">Get in touch with your customers with our responsive website</span></div>
+<div class="business-about">{{HTML::image('assets/business/businesspage_for_wesbite-01.png','alt_eqvialent_in_html',array('class'=>'business-about-img'))}}<span class="business-about-img-caption">We provide analytics for you Use our location-based ads to target customers</span></div>
+<div class="business-about">{{HTML::image('assets/business/businesspage_for_wesbite-04.png','alt_eqvialent_in_html',array('class'=>'business-about-img'))}}<span class="business-about-img-caption">No website ..No worry!! We provide technical support Use clickntoor services with ease</span></div>
+<div class="business-about">{{HTML::image('assets/business/businesspage_for_wesbite-02.png','alt_eqvialent_in_html',array('class'=>'business-about-img'))}}<span class="business-about-img-caption">Grow your business with clickntoor</span></div>
+</div>
+
+
+
+<div id="business-team-container">
+	<h6>Our Partners</h6>
+	<div id="business-team">
+		<div id="per1" class="per">{{HTML::image('assets/careers/team/face.jpg','person',array('class'=>'img-circle','id'=>'businessper-img1'))}}<h5>Person </h5><p>Person  is a..... ........ ......... ............ ...............</p></div>
+		<div id="per2" class="per">{{HTML::image('assets/careers/team/face.jpg','person',array('class'=>'img-circle','id'=>'businessper-img2'))}}<h5>Person </h5><p>Person  is a..... ........ ......... ............ ...............</p></div>
+		<div id="per3" class="per">{{HTML::image('assets/careers/team/face.jpg','person',array('class'=>'img-circle','id'=>'businessper-img3'))}}<h5>Person </h5><p>Person  is a..... ........ ......... ............ ...............</p></div>
+		<div id="per4" class="per">{{HTML::image('assets/careers/team/face.jpg','person',array('class'=>'img-circle','id'=>'businessper-img4'))}}<h5>Person </h5><p>Person  is a..... ........ ......... ............ ...............</p></div>
+		<div id="per5" class="per">{{HTML::image('assets/careers/team/face.jpg','person',array('class'=>'img-circle','id'=>'businessper-img5'))}}<h5>Person </h5><p>Person  is a..... ........ ......... ............ ...............</p></div>
+		<div id="per6" class="per">{{HTML::image('assets/careers/team/face.jpg','person',array('class'=>'img-circle','id'=>'businessper-img6'))}}<h5>Person </h5><p>Person  is a..... ........ ......... ............ ...............</p></div>
+		<div id="per7" class="per">{{HTML::image('assets/careers/team/face.jpg','person',array('class'=>'img-circle','id'=>'businessper-img7'))}}<h5>Person </h5><p>Person  is a..... ........ ......... ............ ...............</p></div>
+		<div id="per8" class="per">{{HTML::image('assets/careers/team/face.jpg','person',array('class'=>'img-circle','id'=>'businessper-img8'))}}<h5>Person</h5><p>Person  is a..... ........ ......... ............ ...............</p></div>
+	
+    </div>
+</div>    
+
+
+
+	</div>
 @stop
--- app/views/pages/careers.blade.php
+++ app/views/pages/careers.blade.php
@@ -2,7 +2,87 @@
 
 @section('title','Join our team')
 @section('content')
-	<div class="cover">
+	<div class="career-cover">
 		<div id="careerscover"></div>
+	
+	 <p id="join-title1">Clickntoor is an initiative to organize hastle free trips to places near you</p>
+
+<div id="join-about-container">
+<div class="join-about-box">{{HTML::image('assets/careers/Carrers_joinus_all_pics-03.png','alt_eqvialent_in_html',array('class'=>'join-about-img'))}}<span class="join-about-img-caption">People that more like friends that just collegues</span></div>
+<div class="join-about-box">{{HTML::image('assets/careers/Carrers_joinus_all_pics-06.png','alt_eqvialent_in_html',array('class'=>'join-about-img'))}}<span class="join-about-img-caption">We care for clent’s, user’s and fellow tooru’s</span></div>
+<div class="join-about-box">{{HTML::image('assets/careers/Carrers_joinus_all_pics-05.png','alt_eqvialent_in_html',array('class'=>'join-about-img'))}}<span class="join-about-img-caption">Discounted tours Result matter than how much vacation we take</span></div>
+<div class="join-about-box">{{HTML::image('assets/careers/Carrers_joinus_all_pics-08.png','alt_eqvialent_in_html',array('class'=>'join-about-img'))}}<span class="join-about-img-caption">Clear growth path where you define where you want to get to</span></div>
+<div class="join-about-box">{{HTML::image('assets/careers/Carrers_joinus_all_pics-01.png','alt_eqvialent_in_html',array('class'=>'join-about-img'))}}<span class="join-about-img-caption">Work from any place Result matter more than where we produce them</span></div>
+<div class="join-about-box">{{HTML::image('assets/careers/Carrers_joinus_all_pics-04.png','alt_eqvialent_in_html',array('class'=>'join-about-img'))}}<span class="join-about-img-caption">No Hierarchy-All are equal Position doesn’t matter every does real work and get hands dirty</span></div>
+<div class="join-about-box">{{HTML::image('assets/careers/Carrers_joinus_all_pics-07.png','alt_eqvialent_in_html',array('class'=>'join-about-img'))}}<span class="join-about-img-caption">Get sh*t done mantra Result matters more than hours we work</span></div>
+<div class="join-about-box">{{HTML::image('assets/careers/Carrers_joinus_all_pics-02.png','alt_eqvialent_in_html',array('class'=>'join-about-img'))}}<span class="join-about-img-caption">Equity for people we want to keep for ever</span></div>
+</div>
+</div>
+
+<div id="culture-container">
+	<h3>Our goal is to cultivate a <b>culture</b> that helps us achieve our ambitions</h3>
+<h5>Here are few things that we ....</h5>
+<div id="culture">
+
+
+<div id="colmn1">
+<h4>Believe in autonomy not autocracy</h4>
+<p>Influence should be independent of hierarchy
+Position doesn’t matter every one does real work and get
+thier hands dirty</p>
+<h4>Transparency</h4>
+<p>Power is gained by sharing knowledge not hoarding it
+We share almost everything.</p>
+<h4>Reasonably picky about our people</h4>
+<p>With great people comes great responsibilities</p>
+<h4>Continue to frequently refactored</h4>
+<p>we love to Improve Internal structure without
+changing the external behaviour</p>
+</div>
+
+<div id="colmn2">
+	{{HTML::image('assets/careers/Eyes-and-Business-15.png','alt_eqvialent_in_html',array('id'=>'colmn2-img'))}}
+</div>
+
+<div id="colmn3">
+	<h4 id="h4para">We value people with</h4><div id="para1">
+<p><span id="bold">H</span>umble - Self aware and Respectable</p>
+<p><span id="bold">E</span>ffective - Measurably moves needle
+Immeasurably add values</p>
+<p><span id="bold">A</span>daptable - Curious and constantly changing</p>
+<p><span id="bold">R</span>emarkable - Smart,Creative,Resourceful</p>
+<p><span id="bold">T</span>ransparent - Open and honest with others
+and with themselves</p></div>
+<h4>Hire to elevate.We don’t just hire to delegate</h4>
+<p id="para2">We’re a team, not a family.So we want stars in every position</p>
+<h4>Love to be different</h4>
+<p id="para3">Remarkble outcomes rarely results from modest risks</p>
+<h4>Work to solve for the customers</h4>
+<p id="para4">“It’s better to obsess over our customers than
+our competitors”</p>
+</div>
+
+</div>
+</div>
+
+
+<div id="team-container">
+	<h6>Our Team</h6>
+	<div id="team">
+		<div id="per1" class="per">{{HTML::image('assets/careers/team/jayasoo.jpg','jayasooryan',array('class'=>'img-circle','id'=>'per-img1'))}}<h5>Jayasooryan K V</h5><p>Person  is a..... ........ ......... ............ ...............</p></div>
+		<div id="per2" class="per">{{HTML::image('assets/careers/team/face.jpg','person',array('class'=>'img-circle','id'=>'per-img2'))}}<h5>Person </h5><p>Person  is a..... ........ ......... ............ ...............</p></div>
+		<div id="per3" class="per">{{HTML::image('assets/careers/team/face.jpg','person',array('class'=>'img-circle','id'=>'per-img3'))}}<h5>Person </h5><p>Person  is a..... ........ ......... ............ ...............</p></div>
+		<div id="per4" class="per">{{HTML::image('assets/careers/team/face.jpg','person',array('class'=>'img-circle','id'=>'per-img4'))}}<h5>Person </h5><p>Person  is a..... ........ ......... ............ ...............</p></div>
+		<div id="per5" class="per">{{HTML::image('assets/careers/team/face.jpg','person',array('class'=>'img-circle','id'=>'per-img5'))}}<h5>Person </h5><p>Person  is a..... ........ ......... ............ ...............</p></div>
+		<div id="per6" class="per">{{HTML::image('assets/careers/team/face.jpg','person',array('class'=>'img-circle','id'=>'per-img6'))}}<h5>Person </h5><p>Person  is a..... ........ ......... ............ ...............</p></div>
+		<div id="per7" class="per">{{HTML::image('assets/careers/team/face.jpg','person',array('class'=>'img-circle','id'=>'per-img7'))}}<h5>Person </h5><p>Person  is a..... ........ ......... ............ ...............</p></div>
+		<div id="per8" class="per">{{HTML::image('assets/careers/team/face.jpg','person',array('class'=>'img-circle','id'=>'per-img8'))}}<h5>Person</h5><p>Person  is a..... ........ ......... ............ ...............</p></div>
+	
+    </div>
+</div>    
+
+
+
+
 	</div>
 @stop
